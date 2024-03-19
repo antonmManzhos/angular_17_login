b620ab1c031d9c2d5d8c5a3ed71630a98d5c99e5
Anton Manzhos <antonm1.manzhos@gmail.com>
2024-03-19 19:11:51 +0200

    Added delete column

--- src/app/pages/to-do-derive-table/to-do-derive-table.component.ts
+++ src/app/pages/to-do-derive-table/to-do-derive-table.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import {ITodoItemViewModel} from "../../../models/ItodoItemViewModel";
 import {ToDoService} from "../../../services/to-do.service";
 import {CdkTableModule} from "@angular/cdk/table";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-to-do-derive-table',
   standalone: true,
-  imports: [CdkTableModule],
+  imports: [CdkTableModule, DatePipe],
   templateUrl: './to-do-derive-table.component.html',
   styleUrl: './to-do-derive-table.component.css'
 })
 export class ToDoDeriveTableComponent {
-  displayedColumns: string[] = ['dateTodoList','titleInput', 'description'];
+  displayedColumns: string[] = ['dateTodoList','titleInput', 'description', 'delete'];
   public toDoArray: ITodoItemViewModel[] = [];
 
     constructor(private toDoService: ToDoService) {
